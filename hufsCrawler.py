5f8a572e47592a5d2d045a60a289ff3b57e4cdee
yuda1124 <young.kim@n3n.io>
2017-08-17 16:43:01 +0900

    make global variable and set target uri

--- hufsCrawler.py
+++ hufsCrawler.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-req = requests.get('http://www.hufs.ac.kr/user/indexSub.action?codyMenuSeq=37079&siteId=hufs&menuType=T&uId=4&sortChar=AB&linkUrl=04_0202.html&mainFrame=right')
+targetUri = 'http://www.hufs.ac.kr/user/indexSub.action?codyMenuSeq=37079&siteId=hufs'
+req = requests.get(targetUri)
 html = req.text
 soup = bs(html, 'html.parser')
 notice_list = soup.select('td.title > a')
